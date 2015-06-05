1d8fa46b5baaf864a81bcdb199837b6f022edd10
Samir Marin <sam070@mail.usask.ca>
2015-06-04 21:44:35 -0700

    UI artist with search & README file

---
+++ README.md
@@ -0,0 +1,1 @@
+As part of Project on udacity course: Developing android apps.. not Complete currently working on project....once complete will add further instruction to readme. 
--- app/src/main/java/com/unachicayelmundo/samirmarin/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/unachicayelmundo/samirmarin/spotifystreamer/MainActivityFragment.java
@@ -2,15 +2,26 @@ package com.unachicayelmundo.samirmarin.spotifystreamer;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private ArrayAdapter<String> adapter;
+
 
     public MainActivityFragment() {
     }
@@ -18,6 +29,60 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> fakeArtist = new ArrayList<String>();
+        String artistA = "Blink-182-All the Small things ";
+        String artistB = "Passenger-All the Little Things ";
+        String artistC = "Franco De vita-Claro que se Perder ";
+        String artistD = "Adrian Marin-Esperanza Del Alma ";
+        for(int i =0; i < 5; i++){
+            fakeArtist.add(artistA + i);
+            fakeArtist.add(artistB + i);
+            fakeArtist.add(artistC + i);
+            fakeArtist.add(artistD + i);
+        }
+        adapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_artist,
+                R.id.list_item_artist_textview,
+                fakeArtist
+        );
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        EditText artistSearch = (EditText) rootView.findViewById(R.id.artist_search);
+
+        artistSearch.addTextChangedListener(makeTextWatcher());
+
+        ListView list_artist_view = (ListView) rootView.findViewById(R.id.listview_artist);
+
+        list_artist_view.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    // filter for when searching artist name... 
+    private TextWatcher makeTextWatcher(){
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+
+        return textWatcher;
+
+
     }
 }
